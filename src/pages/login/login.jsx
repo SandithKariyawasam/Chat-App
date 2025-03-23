62792e3b0aa651f4788bbb36c71589d8ca88b4e8
Sandith Kariyawasam <136833383+SandithKariyawasam@users.noreply.github.com>
2025-03-23 11:57:26 +0530

    done login page

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -7,6 +7,20 @@ const login = () => {
     <div>
       <div className="login">
         <img src={assets.logo_big} alt="" className="logo" />
+        <form className="login-form">
+          <h2>Sign Up</h2>
+          <input type="text" className="form-input" placeholder='Username' required/>
+          <input type="email" className="form-input" placeholder='Email address' required/>
+          <input type="password" className="form-input" placeholder='Password' required/>
+          <button type='submit'>Sign up</button>
+          <div className="login-term">
+            <input type="checkbox" />
+            <p>Agree to the terms of use & privacy policy</p>
+          </div>
+          <div className="login-forgot">
+            <p className="login-toggle">Already have an account <span>click here</span></p>
+          </div>
+        </form>
       </div>
     </div>
   )
